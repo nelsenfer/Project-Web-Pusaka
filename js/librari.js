75723f655082f21025a5db39478e662b13e8a225
nels <nelsenfer17@gmail.com>
2025-01-18 13:35:07 +0700

    embohh

---
+++ js/librari.js
@@ -0,0 +1,14 @@
+let keriss = [
+  {
+    nama: "DAPUR BROJOL",
+    isi: "Sampai saat ini, beberapa masyarakat meyakini bahwa keris brojol manfaatnya sangat besar dalam proses kelahiran. Padahal, keris ini memiliki filosofi penting tentang kehidupan sekaligus sejumlah makna spiritual yang dapat dijadikan sebagai pegangan hidup.Tidak hanya untuk proses kelahiran, filosofi keris brojol diharapkan pada pemilik keris ini dapat dilancarkan secara urusannya, termasuk lancar dalam mencari rezeki.",
+    img: "../img/DAPUR BROJOL.jpg",
+  },
+  {
+    nama: "keris dua",
+    isi: "ini adalah keris dua",
+    img: "../img/keris 2.jpg",
+  },
+];
+
+export { keriss };
--- js/tes.js
+++ js/tes.js
@@ -2,14 +2,21 @@ const keriss = [
   {
     nama: "DAPUR BROJOL",
     isi: "Sampai saat ini, beberapa masyarakat meyakini bahwa keris brojol manfaatnya sangat besar dalam proses kelahiran. Padahal, keris ini memiliki filosofi penting tentang kehidupan sekaligus sejumlah makna spiritual yang dapat dijadikan sebagai pegangan hidup.Tidak hanya untuk proses kelahiran, filosofi keris brojol diharapkan pada pemilik keris ini dapat dilancarkan secara urusannya, termasuk lancar dalam mencari rezeki.",
-    img: "img/DAPUR BROJOL.jpg",
+    img: "../img/DAPUR BROJOL.jpg",
   },
   {
     nama: "keris dua",
     isi: "ini adalah keris dua",
-    img: "img/keris 2.jpg",
+    img: "../img/keris 2.jpg",
   },
 ];
+
+//iju coba card
+
+// import { keriss } from "./librari.js";
+
+console.log(keriss);
+
 let currentSlide = 0;
 
 function plusDivs(n) {
@@ -59,3 +66,5 @@ document.addEventListener("DOMContentLoaded", function () {
         <button onclick="plusDivs(1)">Next ❯</button>`;
   slideitem.appendChild(buttonContainer);
 });
+
+//daftar card
